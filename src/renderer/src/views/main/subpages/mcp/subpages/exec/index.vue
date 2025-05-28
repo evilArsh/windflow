79c332d01116d57a6cd304032c25ff11aef3a82b
EvilArsh <arshdebian@163.com>
2025-05-28 21:25:52 +0800

    feat(mcp): 添加 npm 镜像设置功能
    - 在英文和中文语言文件中添加了 npm 镜像设置相关翻译
    - 实现了 npm 镜像设置的前端界面，包括选择框和选项
    - 添加了相关的脚本逻辑，用于处理 npm 镜像的选择

--- src/renderer/src/views/main/subpages/mcp/subpages/exec/index.vue
+++ src/renderer/src/views/main/subpages/mcp/subpages/exec/index.vue
@@ -1,5 +1,42 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { t } = useI18n()
+const npmMirrors = ref([
+  { label: "taobao", value: "https://registry.npmmirror.com" },
+  { label: "npm", value: "https://registry.npmjs.org" },
+])
+const settings = reactive({
+  npmMirror: "https://registry.npmmirror.com",
+})
+</script>
 <template>
-  <el-empty></el-empty>
+  <ContentLayout>
+    <div class="mx-auto my-1rem max-w-1024px flex flex-col gap1rem">
+      <ContentBox class="p1rem!">
+        <div class="flex-1">
+          <el-text type="primary" size="large">{{ t("mcp.settings.npmMirror") }}</el-text>
+        </div>
+        <template #end>
+          <el-select v-model="settings.npmMirror" class="w-35rem!">
+            <el-option
+              v-for="item in npmMirrors"
+              :key="item.value"
+              v-model="settings.npmMirror"
+              :label="item.value"
+              :value="item.value">
+              <div class="flex items-center">
+                <el-tag class="mr-1rem w-5rem" size="small">
+                  {{ item.label }}
+                </el-tag>
+                <span>{{ item.value }}</span>
+              </div>
+            </el-option>
+          </el-select>
+        </template>
+        <template #footer>
+          <el-text size="small">啊实打实的</el-text>
+        </template>
+      </ContentBox>
+    </div>
+  </ContentLayout>
 </template>
 <style lang="scss" scoped></style>
